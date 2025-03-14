8d75b2e12fb0dda27353f08acea4211c5b1cd5cd
unknown <locodrd1@hotmail.com>
2025-03-14 18:11:33 -0500

    se sube query de actividad 2 analisis de datos

--- analisis_de_datos/actividad2/Actividad2.sql
+++ analisis_de_datos/actividad2/Actividad2.sql
@@ -1,18 +1,23 @@
+
+#------------------------------------------------------------------------------------#
 /* Traer todas las canciónes de la banda AC/DC    */
 select album.Title as album,track.Name,artis.Name as artistName  from artist artis
 inner join album on album.ArtistId=artis.Artistid
 inner join track on track.AlbumId=album.Albumid
 where artis.name="AC/DC";
+#------------------------------------------------------------------------------------#
 
 
+#------------------------------------------------------------------------------------#
 /*  Traer todas las canciones del género de Rock    */
-
 select album.Title as album,track.Name,artis.Name as artistName  from artist artis
 inner join album on album.ArtistId=artis.Artistid
 inner join track on track.AlbumId=album.Albumid
 inner join Genre on Genre.GenreId=track.GenreId
 where Genre.name="Rock";
+#------------------------------------------------------------------------------------#
 
+#------------------------------------------------------------------------------------#
 /*  Traer todos los clientes que compraron canciones de Rock */
 SELECT DISTINCT c.CustomerId, c.FirstName, c.LastName, c.Email
 FROM Customer c
@@ -22,8 +27,10 @@ JOIN Track t ON il.TrackId = t.TrackId
 JOIN Genre g ON t.GenreId = g.GenreId
 WHERE g.Name = 'Rock'
 ORDER BY c.LastName, c.FirstName;
+#------------------------------------------------------------------------------------#
 
 
+#------------------------------------------------------------------------------------#
 /*  Traer la cantidad de canciones compradas por cada cliente  */
 SELECT 
     c.CustomerId, 
@@ -36,6 +43,7 @@ JOIN Invoice i ON c.CustomerId = i.CustomerId
 JOIN InvoiceLine il ON i.InvoiceId = il.InvoiceId
 GROUP BY c.CustomerId, c.FirstName, c.LastName, c.Email
 ORDER BY total_songs_purchased DESC;
+#------------------------------------------------------------------------------------#
 
 
 
